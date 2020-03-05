0d887eed68d4ac828bd0afabb15c548cffbdfccf
Terry Tay <txrrythk@gmail.com>
2020-03-05 22:24:05 +0800

    Add Location class and LocationTest

--- src/main/java/Location.java
+++ src/main/java/Location.java
@@ -1,2 +1,41 @@
 public class Location {
+    private String name;
+    private String address;
+    private String faculty;
+    private boolean hasPort;
+    private boolean isIndoor;
+    private int maxPax;
+
+    public Location(String name, String address, String faculty, boolean hasPort, boolean isIndoor, int maxPax) {
+        this.name = name;
+        this.address = address;
+        this.faculty = faculty;
+        this.hasPort = hasPort;
+        this.isIndoor = isIndoor;
+        this.maxPax = maxPax;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public boolean hasPort() {
+        return hasPort;
+    }
+
+    public boolean isIndoor() {
+        return isIndoor;
+    }
+
+    public int getMaxPax() {
+        return maxPax;
+    }
 }
--- src/test/java/LocationTest.java
+++ src/test/java/LocationTest.java
@@ -1,5 +1,39 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LocationTest {
 
+    Location location1 = new Location("mac commons", "utown", "nil", true, true, 5);
+
+    @Test
+    void getName() {
+        assertEquals("mac commons", location1.getName());
+    }
+
+    @Test
+    void getAddress() {
+        assertEquals("utown", location1.getAddress());
+    }
+
+    @Test
+    void getFaculty() {
+        assertEquals("nil", location1.getFaculty());
+    }
+
+    @Test
+    void hasPort() {
+        assertEquals(true, location1.hasPort());
+    }
+
+    @Test
+    void isIndoor() {
+        assertEquals(true, location1.isIndoor());
+    }
+
+    @Test
+    void getMaxPax() {
+        assertEquals(5, location1.getMaxPax());
+    }
 }
