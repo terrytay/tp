c17da9d6c0e7f41fa145dcd6c8a08f4bddc151cd
GanapathySanathBalaji <e0311091@u.nus.edu>
2020-03-06 23:34:35 +0800

    Edit Duke.java to support new commands.

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,10 +1,27 @@
 package seedu.duke;
 
-import studyarea.StudyArea;
+import event.EventList;
+import ui.Ui;
 
 import java.util.Scanner;
 
 public class Duke {
+
+    public static Ui ui = new Ui();
+
+    public static final String ADD_COMMAND = "add";
+    public static final String VIEW_COMMAND = "view";
+    public static final String PRIORITY_VIEW_COMMAND = "priority_view";
+    public static final String COUNTDOWN_VIEW_COMMAND = "countdown";
+    public static final String CLEAR_COMMAND = "clear";
+    public static final String SEARCH_COMMAND = "search";
+    public static final String DELETE_COMMAND = "delete";
+    public static final String INVALID_INDEX_MESSAGE = "Enter a valid index";
+    public static final String INVALID_COMMAND_MESSAGE = "Enter a valid command";
+    public static final String BYE_COMMAND = "bye";
+    public static final String BYE_MESSAGE = "Bye!!!!!!";
+    static EventList eventList = new EventList();
+
     /**
      * Main entry-point for the java.duke.Duke application.
      */
@@ -19,8 +36,55 @@ public class Duke {
 
         Scanner in = new Scanner(System.in);
         System.out.println("Hello " + in.nextLine());
-        StudyArea studyArea = new StudyArea();
-        String output = studyArea.getLocationsList().get(1).getAddress();
-        System.out.println(output);
+        runCommands();
+    }
+
+    private static void runCommands() {
+        Scanner in = new Scanner(System.in);
+        String command;
+        command = in.nextLine();
+        while (!command.equals(BYE_COMMAND)) {
+            try {
+                String commandType = command.split(" ")[0];
+                switch (commandType) {
+                case ADD_COMMAND:
+                    eventList.add(command);
+                    break;
+                case VIEW_COMMAND:
+                    eventList.listEvents();
+                    break;
+                case PRIORITY_VIEW_COMMAND:
+                    eventList.priorityView();
+                    break;
+                case COUNTDOWN_VIEW_COMMAND:
+                    eventList.countdownView();
+                    break;
+                case CLEAR_COMMAND:
+                    eventList.clearEvents();
+                    break;
+                case SEARCH_COMMAND:
+                    eventList.searchEvents(command.split(" ", 2)[1]);
+                    break;
+                case DELETE_COMMAND:
+                    eventList.deleteEvent(Integer.parseInt(command.split(" ", 2)[1]));
+                    break;
+                default:
+                    ui.printLine();
+                    System.out.println(INVALID_COMMAND_MESSAGE);
+                    ui.printLine();
+                    break;
+                }
+            } catch (NumberFormatException e) {
+                ui.printLine();
+                System.out.println(INVALID_INDEX_MESSAGE);
+                ui.printLine();
+            } catch (IndexOutOfBoundsException e) {
+                ui.printLine();
+                System.out.println(INVALID_COMMAND_MESSAGE);
+                ui.printLine();
+            }
+            command = in.nextLine();
+        }
+        System.out.println(BYE_MESSAGE);
     }
 }
