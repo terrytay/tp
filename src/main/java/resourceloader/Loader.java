4f0bce39fecef4a794f9212d352fdecf10a95bad
Terry Tay <txrrythk@gmail.com>
2020-03-06 11:38:10 +0800

    Javadoc not added for resourceloader yet

--- src/main/java/resourceloader/Loader.java
+++ src/main/java/resourceloader/Loader.java
@@ -1,4 +1,17 @@
 package resourceloader;
 
-public class Loader {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * Loader is a resource loader for all the external data that is within our java program.
+ * Examples of resources will be task data, study locations etc..
+ * Loader is an interface that enables subsequent loaders to be made and disciplined,
+ * i.e. locationsLoader, organiserLoader..
+ */
+public interface Loader {
+    public void loadFile() throws FileNotFoundException;
+
+    public File getFile();
 }
--- src/main/java/resourceloader/LocationLoader.java
+++ src/main/java/resourceloader/LocationLoader.java
@@ -1,4 +1,25 @@
 package resourceloader;
 
-public class LocationLoader {
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class LocationLoader implements Loader {
+    private final String url;
+    private File file;
+
+    public LocationLoader(String url) {
+        this.url = url;
+    }
+
+    public void loadFile() throws FileNotFoundException {
+        try {
+            file = new File(url);
+        } catch (Exception e) {
+            throw new FileNotFoundException("File not found.");
+        }
+    }
+
+    public File getFile() {
+        return this.file;
+    }
 }
