e1692e5eb418b2d6ce2be5e763b9ee4fb55d2c6a
NizarMohd <mohdnizar97@gmail.com>
2020-03-15 13:23:08 +0800

    transfer Logo to Constants file

--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -7,35 +7,7 @@ import java.io.PrintStream;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 import static java.lang.System.lineSeparator;
-import static ui.Constants.BYE_COMMAND;
-import static ui.Constants.DAB;
-import static ui.Constants.GOODBYE_MESSAGE;
-import static ui.Constants.HELP_DESCRIPTION_1;
-import static ui.Constants.HELP_DESCRIPTION_10;
-import static ui.Constants.HELP_DESCRIPTION_11;
-import static ui.Constants.HELP_DESCRIPTION_12;
-import static ui.Constants.HELP_DESCRIPTION_13;
-import static ui.Constants.HELP_DESCRIPTION_14;
-import static ui.Constants.HELP_DESCRIPTION_15;
-import static ui.Constants.HELP_DESCRIPTION_16;
-import static ui.Constants.HELP_DESCRIPTION_17;
-import static ui.Constants.HELP_DESCRIPTION_18;
-import static ui.Constants.HELP_DESCRIPTION_19;
-import static ui.Constants.HELP_DESCRIPTION_2;
-import static ui.Constants.HELP_DESCRIPTION_20;
-import static ui.Constants.HELP_DESCRIPTION_3;
-import static ui.Constants.HELP_DESCRIPTION_4;
-import static ui.Constants.HELP_DESCRIPTION_5;
-import static ui.Constants.HELP_DESCRIPTION_7;
-import static ui.Constants.HELP_DESCRIPTION_8;
-import static ui.Constants.HELP_DESCRIPTION_9;
-import static ui.Constants.LINE;
-import static ui.Constants.MAX_LINE_LENGTH;
-import static ui.Constants.SPACE;
-import static ui.Constants.START_MESSAGE;
-import static ui.Constants.STUDY_COMMAND;
-import static ui.Constants.TAB;
-import static ui.Constants.TASK_COMMAND;
+import static ui.Constants.*;
 
 /**
  * Contains functions used to interact with the user.
@@ -134,36 +106,7 @@ public class Ui {
      * Display welcome message.
      */
     public void printWelcomeMessage() {
-
-        //@@author NizarMohd-reused
-        //Reused logo from http://patorjk.com/software/taag/#p=display&f=Graffiti&t=TypeSomething with minor
-        // modifications. The website above allows for generation of fonts in ascii format. Therefore, logo is
-        // generated from the service provided by the website.
-        String logo = "_______/\\\\\\\\\\_____________________________________________________/\\\\\\\\\\_____/\\\\\\__"
-                + "__________________________________________/\\\\\\____________\n"
-                + "______/\\\\\\///\\\\\\__________________________________________________\\/\\\\\\\\\\\\___\\/\\\\\\_"
-                + "_________________________________________/\\\\\\\\\\\\\\_________\n"
-                + "_____/\\\\\\/__\\///\\\\\\____________________/\\\\\\\\\\\\\\\\___________________\\/\\\\\\/\\\\\\_"
-                + "_\\/\\\\\\___/\\\\\\__________________________________/\\\\\\\\\\\\\\\\\\_______\n"
-                + "_____/\\\\\\______\\//\\\\\\___/\\\\/\\\\\\\\\\\\\\____/\\\\\\////\\\\\\___/\\\\\\\\\\\\\\\\\\___"
-                + "__\\/\\\\\\//\\\\\\_\\/\\\\\\__\\///_______/\\\\\\\\\\\\\\\\______/\\\\\\\\\\\\\\\\__"
-                + "_\\//\\\\\\\\\\\\\\_______\n"
-                + "_____\\/\\\\\\_______\\/\\\\\\__\\/\\\\\\/////\\\\\\__\\//\\\\\\\\\\\\\\\\\\__\\////////\\\\\\___"
-                + "_\\/\\\\\\\\//\\\\\\\\/\\\\\\___/\\\\\\____/\\\\\\//////_____/\\\\\\/////\\\\\\___\\//\\\\\\\\\\____"
-                + "___\n"
-                + "______\\//\\\\\\______/\\\\\\___\\/\\\\\\___\\///____\\///////\\\\\\____/\\\\\\\\\\\\\\\\\\\\__"
-                + "_\\/\\\\\\_\\//\\\\\\/\\\\\\__\\/\\\\\\___/\\\\\\___________/\\\\\\\\\\\\\\\\\\\\\\_____\\//\\\\\\__"
-                + "_____\n"
-                + "________\\///\\\\\\__/\\\\\\_____\\/\\\\\\___________/\\\\_____\\\\\\___/\\\\\\/////\\\\\\__"
-                + "_\\/\\\\\\__\\//\\\\\\\\\\\\__\\/\\\\\\__\\//\\\\\\_________\\//\\\\///////_______\\///_______\n"
-                + "____________\\///\\\\\\\\\\/______\\/\\\\\\__________\\//\\\\\\\\\\\\\\\\__"
-                + "_\\//\\\\\\\\\\\\\\\\/\\\\"
-                + "_\\/\\\\\\___\\//\\\\\\\\\\__\\/\\\\\\___\\///\\\\\\\\\\\\\\\\___\\//\\\\\\\\\\\\\\\\\\\\_____"
-                + "_/\\\\\\_____\n"
-                + "______________\\/////________\\///____________\\////////_____\\////////\\//___\\///_____\\/////__"
-                +  "_\\///______\\////////_____\\//////////______\\///_____";
-        //@@author
-        this.out.println("Hello from\n" + logo);
+        this.out.println("Hello from\n" + LOGO);
         this.out.println("What is your name?");
         printLine();
         String name = this.in.nextLine();
