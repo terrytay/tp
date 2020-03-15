3d7d63855fd5dc6914a6d4819729f193458254ac
NizarMohd <mohdnizar97@gmail.com>
2020-03-15 17:04:32 +0800

    rectify changes

--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -7,7 +7,37 @@ import java.io.PrintStream;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 import static java.lang.System.lineSeparator;
-import static ui.Constants.*;
+import static ui.Constants.BYE_COMMAND;
+import static ui.Constants.DAB;
+import static ui.Constants.FLAGS;
+import static ui.Constants.GOODBYE_MESSAGE;
+import static ui.Constants.HELP_DESCRIPTION_1;
+import static ui.Constants.HELP_DESCRIPTION_10;
+import static ui.Constants.HELP_DESCRIPTION_11;
+import static ui.Constants.HELP_DESCRIPTION_12;
+import static ui.Constants.HELP_DESCRIPTION_13;
+import static ui.Constants.HELP_DESCRIPTION_14;
+import static ui.Constants.HELP_DESCRIPTION_15;
+import static ui.Constants.HELP_DESCRIPTION_16;
+import static ui.Constants.HELP_DESCRIPTION_17;
+import static ui.Constants.HELP_DESCRIPTION_18;
+import static ui.Constants.HELP_DESCRIPTION_19;
+import static ui.Constants.HELP_DESCRIPTION_2;
+import static ui.Constants.HELP_DESCRIPTION_20;
+import static ui.Constants.HELP_DESCRIPTION_3;
+import static ui.Constants.HELP_DESCRIPTION_4;
+import static ui.Constants.HELP_DESCRIPTION_5;
+import static ui.Constants.HELP_DESCRIPTION_7;
+import static ui.Constants.HELP_DESCRIPTION_8;
+import static ui.Constants.HELP_DESCRIPTION_9;
+import static ui.Constants.LINE;
+import static ui.Constants.LOGO;
+import static ui.Constants.MAX_LINE_LENGTH;
+import static ui.Constants.SPACE;
+import static ui.Constants.START_MESSAGE;
+import static ui.Constants.STUDY_COMMAND;
+import static ui.Constants.TAB;
+import static ui.Constants.TASK_COMMAND;
 
 /**
  * Contains functions used to interact with the user.
@@ -120,7 +150,7 @@ public class Ui {
      * This method prints the list of supported flags for the User.
      */
 
-    public void printStudyAreaHelp(){
+    public void printStudyAreaHelp() {
         this.out.println(FLAGS);
     }
 
