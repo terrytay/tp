4920354f75a3d68f97216e78c5dec3e1374b0d2b
NizarMohd <mohdnizar97@gmail.com>
2020-03-28 15:06:01 +0800

    edit exceptions to pass test

--- src/main/java/exception/command/IndexNotIntegerException.java
+++ src/main/java/exception/command/IndexNotIntegerException.java
@@ -5,7 +5,7 @@ import exception.CustomException;
 //@@author GanapathySanathBalaji
 public class IndexNotIntegerException extends CustomException {
     public IndexNotIntegerException() {
-        super("Enter a valid integer index (1-based) to delete/edit the corresponding event "
+        super("Enter a valid integer index (1-based) to delete/edit/done the corresponding event "
                 + "(format: delete/edit/done index)");
     }
 }
