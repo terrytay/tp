9c5fe03752086a320a0dd1222f4bad041b82a6aa
NizarMohd <mohdnizar97@gmail.com>
2020-04-07 09:33:35 +0800

    edit authorship, add studyArea tests

--- src/test/java/studyarea/StudyAreaTest.java
+++ src/test/java/studyarea/StudyAreaTest.java
@@ -1,8 +1,12 @@
 package studyarea;
 
 import org.junit.jupiter.api.Test;
+import ui.Ui;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static java.lang.System.lineSeparator;
+import static org.junit.jupiter.api.Assertions.*;
+import static ui.Constants.MAX_LINE_LENGTH;
+import static ui.Constants.TAB;
 
 //@@author terrytay
 class StudyAreaTest {
@@ -38,5 +42,41 @@ class StudyAreaTest {
     @Test
     void getName() {
         assertEquals("mac commons", location1.getName());
+    }
+
+
+    //@@author NizarMohd
+    @Test
+    void testToString() {
+       String out = "\t __________________________________________________________\n" +
+               "\t Name: mac commons\n" +
+               "\t Address: utown\n" +
+               "\t Faculty: nil\n" +
+               "\t Port: true\n" +
+               "\t Indoor: true\n" +
+               "\t Maximum number of Pax: 5\n" +
+               "\t __________________________________________________________";
+        assertEquals(out, location1.toString());
+    }
+
+    @Test
+    void containsSearchKey_assertTrue() {
+        assertTrue(location1.containsSearchKey("utown"));
+    }
+
+    @Test
+    void containsSearchKey_assertFalse() {
+        assertFalse(location1.containsSearchKey("u-town"));
+    }
+
+
+    @Test
+    void isSizeCapable_assertTrue() {
+        assertTrue(location1.isSizeCapable("3"));
+    }
+
+    @Test
+    void isSizeCapable_assertFalse() {
+        assertFalse(location1.isSizeCapable("10"));
     }
 }
