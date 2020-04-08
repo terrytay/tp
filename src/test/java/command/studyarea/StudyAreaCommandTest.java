e14d4a82c2abd7f5c29303949c248e65b5af4d3b
NizarMohd <mohdnizar97@gmail.com>
2020-04-08 21:04:24 +0800

    edit to pass checkstyle

--- src/test/java/command/studyarea/StudyAreaCommandTest.java
+++ src/test/java/command/studyarea/StudyAreaCommandTest.java
@@ -14,19 +14,24 @@ import static ui.Constants.WRONG_FLAG_USAGE;
 
 class StudyAreaCommandTest {
 
-
+    /**
+     * throws an exception pertaining to blank commands.
+     */
     @Test
     void validateCommand_throwsException_blankCommand() {
         try {
-                String command = "";
-                command = StudyAreaCommand.validateCommand(command);
-                fail("should have raised an exception");
+            String command = "";
+            command = StudyAreaCommand.validateCommand(command);
+            fail("should have raised an exception");
         } catch (Exception e) {
             assertEquals(EMPTY_LOCATION, e.getMessage());
         }
 
     }
 
+    /**
+     * throws an exception pertaining to short commands.
+     */
     @Test
     void validateCommand_throwsException_shortCommand() {
         try {
@@ -38,9 +43,11 @@ class StudyAreaCommandTest {
         }
     }
 
+    /**
+     * throws an exception pertaining to wrong commands.
+     */
     @Test
     void filterCommand_throwsException_wrongCommand() {
-
         try {
             String command = "-z";
             boolean b = StudyAreaCommand.filterCommand(command, new StudyAreaList(new ArrayList<>()), new Ui());
